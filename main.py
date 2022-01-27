cb15f31ac403a05f7fe7269ed07ebee12acc3711
ponjae <ponjae11@gmail.com>
2022-01-27 15:59:08 +0100

    Added functionality to display dummy blog posts on the home page, used npoint to generate an API npoint to use. Also updated to a more personal touch

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+from post import Post
+import requests
 
 app = Flask(__name__)
 
 
+def _generate_posts():
+    response = requests.get(
+        "https://api.npoint.io/a09d1034f2e7bbe70b6e")
+    data = response.json()
+    posts = []
+    for post in data:
+        posts.append(Post(post["id"], post["title"],
+                     post["subtitle"], post["body"],
+                     post["author"], post["date"]))
+    return posts
+
+
+posts = _generate_posts()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/about.html")
